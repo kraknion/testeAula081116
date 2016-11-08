2f618762cd4637c44e5b38ba6b9e9481dbb26983
UserPL022Pc05 <UserPL022Pc05@SLFRPL022PC05.ensino.atec.local>
2016-11-08 11:21:10 +0000

    testeAula081116

---
+++ src/game/player.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author UserPL022Pc05
+ */
+public class player extends sensibleDummy{
+    
+    int fatigue;
+    int atack;
+    int defense;
+    char faceTo;
+    
+   public void pass(){
+       
+   }
+   public void shoot(){
+       
+   }
+}
---
+++ src/game/sensibleDummy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author UserPL022Pc05
+ */
+public abstract class sensibleDummy {
+    
+    String nome;
+    String cor;
+    
+    public void move (int x, int y){
+        
+    }
+    
+}
